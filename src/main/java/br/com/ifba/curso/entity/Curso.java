af56436f94c249b00ed4c48a85e9d57aac7f7d50
Luid Emanuel Santos Dos Anjos <luidsmyth@gmail.com>
2026-05-02 18:02:44 -0300

    Jframe e xml feitos, faltando apenas a lógica

---
+++ src/main/java/br/com/ifba/curso/entity/Curso.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.curso.entity;
+
+
+
+
+/**
+ *
+ * @author LUID1.7
+ */
+
+public class Curso {
+    
+}
